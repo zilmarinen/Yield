344cf59d67fd250aaaf9d4011a1aa6535c7f33a9
Zack Brown <zack@zackbrown.co.uk>
2021-10-30 12:06:32 +0100

    Add app view model

--- Yield/View Models/AppViewModel.swift
+++ Yield/View Models/AppViewModel.swift
@@ -15,6 +15,8 @@ class AppViewModel: ObservableObject {
         case exporting(progress: Progress)
     }
     
+    @ObservedObject var editorModel = EditorViewModel()
+    
     @Published var viewState: ViewState = .editing
     
     init() {
@@ -43,6 +45,44 @@ class AppViewModel: ObservableObject {
         catch {
             
             throw error
+        }
+    }
+}
+
+extension AppViewModel {
+    
+    func export() {
+        
+        let panel = NSOpenPanel()
+        
+        panel.allowsMultipleSelection = false
+        panel.canChooseDirectories = true
+        panel.canChooseFiles = false
+        panel.canCreateDirectories = true
+        panel.prompt = "Export"
+        
+        switch panel.runModal() {
+            
+        case .OK:
+            
+            guard let url = panel.url else { return }
+            
+//            let exportOperation = TilemapExportOperation(url: url, tilemap: tilemap)
+//            let writeOperation = WriteOperation(url: url);
+//
+//            let progress = exportOperation.passesResult(to: writeOperation).enqueueWithProgress(on: operationQueue) { result in
+//
+//                DispatchQueue.main.async { [weak self] in
+//
+//                    guard let self = self else { return }
+//
+//                    self.viewState = .idle
+//                }
+//            }
+            
+            //viewState = .exporting(progress: progress)
+            
+        default: break
         }
     }
 }
