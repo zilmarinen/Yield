c0eaa1c2246e2659f0c34924a6fece869afc9ebd
Zack Brown <zack@zackbrown.co.uk>
2021-11-07 14:45:05 +0000

    Update color palettes

--- Yield/Prototypes/ColorPalette.swift
+++ Yield/Prototypes/ColorPalette.swift
@@ -11,30 +11,30 @@ struct ColorPalette {
     let primary: Color
     let secondary: Color
     let tertiary: Color
-    let quaterniary: Color
+    let quaternary: Color
 }
 
 extension ColorPalette {
     
-    static let air = ColorPalette(primary: .clear, secondary: .clear, tertiary: .clear, quaterniary: .clear)
+    static let air = ColorPalette(primary: .clear, secondary: .clear, tertiary: .clear, quaternary: .clear)
     
     static let dirt = ColorPalette(primary: Color(0.776, 0.835, 0.494),
                                    secondary: Color(0.807, 0.989, 0.815),
                                    tertiary: Color(0.764, 0.407, 0.223),
-                                   quaterniary: Color(0.670, 0.427, 0.137))
+                                   quaternary: Color(0.670, 0.427, 0.137))
     
-    static let sand = ColorPalette(primary: Color(0.776, 0.835, 0.494),
-                                   secondary: Color(0.807, 0.989, 0.815),
-                                   tertiary: Color(0.764, 0.407, 0.223),
-                                   quaterniary: Color(0.670, 0.427, 0.137))
+    static let sand = ColorPalette(primary: Color(0.952, 0.874, 0.537),
+                                   secondary: Color(0.941, 0.768, 0.247),
+                                   tertiary: Color(0.933, 0.69, 0.027),
+                                   quaternary: Color(0.952, 0.878, 0.631))
     
     static let stone = ColorPalette(primary: Color(0.392, 0.423, 0.372),
                                     secondary: Color(0.329, 0.337, 0.333),
                                     tertiary: Color(0.439, 0.345, 0.321),
-                                    quaterniary: Color(0.313, 0.231, 0.239))
+                                    quaternary: Color(0.313, 0.231, 0.239))
     
     static let undergrowth = ColorPalette(primary: Color(0.776, 0.835, 0.494),
                                           secondary: Color(0.807, 0.989, 0.815),
                                           tertiary: Color(0.764, 0.407, 0.223),
-                                          quaterniary: Color(0.670, 0.427, 0.137))
+                                          quaternary: Color(0.670, 0.427, 0.137))
 }
