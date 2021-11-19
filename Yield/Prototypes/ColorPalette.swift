a87a6cf02e240c24d19c5da13d8bf07ed55a60b8
Zack Brown <zack@zackbrown.co.uk>
2021-11-19 16:13:10 +0000

    Update color palettes

--- Yield/Prototypes/ColorPalette.swift
+++ Yield/Prototypes/ColorPalette.swift
@@ -33,8 +33,8 @@ extension ColorPalette {
                                     tertiary: Color(0.439, 0.345, 0.321),
                                     quaternary: Color(0.313, 0.231, 0.239))
     
-    static let undergrowth = ColorPalette(primary: Color(0.776, 0.835, 0.494),
-                                          secondary: Color(0.807, 0.989, 0.815),
-                                          tertiary: Color(0.764, 0.407, 0.223),
-                                          quaternary: Color(0.670, 0.427, 0.137))
+    static let undergrowth = ColorPalette(primary: Color(0.541, 0.525, 0.207),
+                                          secondary: Color(0.196, 0.313, 0.180),
+                                          tertiary: Color(0.282, 0.203, 0.203),
+                                          quaternary: Color(0.176, 0.141, 0.141))
 }
