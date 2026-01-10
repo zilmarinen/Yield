07bca20e2895ae572bc2f5b1fbe1e5607155776f
Zack Brown <zack@zackbrown.co.uk>
2026-01-10 13:06:43 +0000

    Implement asset cache mesh and resource loading

--- Sources/Yield/AssetCache.swift
+++ Sources/Yield/AssetCache.swift
@@ -4,8 +4,10 @@
 //  Created by Zack Brown on 05/01/2026.
 //
 
+import AppKit
 import Euclid
 import Foundation
+import RealityKit
 
 @MainActor
 public final class AssetCache {
@@ -13,20 +15,44 @@ public final class AssetCache {
     public static let shared = AssetCache()
     
     internal var meshes: [Asset : Mesh] = [:]
+    internal var resources: [Asset : MeshResource] = [:]
 }
 
 extension AssetCache {
     
-    public func load(asset: Asset) throws -> Mesh {
+    public func load(mesh asset: Asset) throws -> Mesh {
         
         if let mesh = meshes[asset] {
             
             return mesh
         }
         
-        guard let path = Bundle.main.path(forResource: asset.id,
-                                          ofType: "mesh") else { throw CocoaError(.fileNoSuchFile) }
+        guard let handle = NSDataAsset(name: asset.id,
+                                       bundle: .module) else { throw CocoaError(.fileNoSuchFile) }
         
-        return .empty
+        guard let obj = String(data: handle.data,
+                               encoding: .utf8) else { throw CocoaError(.fileReadUnknownStringEncoding) }
+        
+        guard let mesh = Mesh(objString: obj) else { throw CocoaError(.fileReadUnsupportedScheme) }
+        
+        meshes[asset] = mesh
+        
+        return mesh
+    }
+    
+    public func load(resource asset: Asset) throws -> MeshResource {
+        
+        if let resource = resources[asset] {
+            
+            return resource
+        }
+        
+        let mesh = try load(mesh: asset)
+        
+        let resource = MeshResource(mesh: mesh)
+        
+        resources[asset] = resource
+        
+        return resource
     }
 }
